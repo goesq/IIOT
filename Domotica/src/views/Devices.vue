f0795d7e26a294787ecd6ff3bb5bea638bd28dd8
vicgoes <158624036+goesq@users.noreply.github.com>
2025-02-20 22:23:10 -0300

    add devices

--- Domotica/src/views/Devices.vue
+++ Domotica/src/views/Devices.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+    import { Places } from "@/models/devices";
+    import { reactive, ref   } from 'vue';
 
-    
+    const sala: Places = {
+        nome: 'Sala de Estar',
+        devices: []
+    }
+
+    const contador = ref(0);
+    const somar = ()=> contador.value += 1;
 </script>
 
 <template>
-
+    <h1> Contagem: {{ contador }}</h1>
+    <button @click="somar">Somar</button>
 </template>
 
 <style scoped lang="scss">
