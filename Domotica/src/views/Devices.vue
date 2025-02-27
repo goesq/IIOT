5faf3a20722fe17517ee79581fcb6b9adbe8e215
vicgoes <158624036+goesq@users.noreply.github.com>
2025-02-27 19:26:45 -0300

    Update Devices.vue

--- Domotica/src/views/Devices.vue
+++ Domotica/src/views/Devices.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-    import { Places } from "@/models/devices";
+    import { Environment } from "@/models/devices";
     import { reactive, ref   } from 'vue';
 
-    const sala: Places = {
-        nome: 'Sala de Estar',
+    const sala: Environment = {
+        name: 'Sala de Estar',
         devices: []
     }
 </script>
 
 <template>
-    
+
 </template>
 
 <style scoped lang="scss">
