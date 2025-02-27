c495c77de05bb20117d0dcebb0d02cb13ff34e94
vicgoes <158624036+goesq@users.noreply.github.com>
2025-02-27 19:46:21 -0300

    att

---
+++ Domotica/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1> Page Not Found 😒</h1>
+    <img src="https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-2048x998-yjzeuy4v.png"></img>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
