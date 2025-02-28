7680a382fa100378752e461608fcebb18dc00efd
vicgoes <158624036+goesq@users.noreply.github.com>
2025-02-27 21:39:28 -0300

    .

--- Domotica/src/views/Devices.vue
+++ Domotica/src/views/Devices.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
     import { Environment } from "@/models/devices";
-    import { reactive, ref   } from 'vue';
-
+    import { reactive, ref} from 'vue';
+    import { Device } from "@/models/devices";
 
     const environments: Array<Environment> = reactive([]);
-    const sala: Environment = reactive(new Environment());
     
     const ar: Device = reactive(new Device());
     ar.name = 'Ar condicionado';
@@ -19,13 +18,34 @@
     iluminacao.name = 'Iluminação';
     iluminacao.color = '#00eddd'
 
+    const sala: Environment = reactive(new Environment());
+    sala.name = 'Sala';
+    sala.devices = [ar,tv,iluminacao];
+    //sala.devices.push(ar);
+    //sala.devices.push(tv);
+    //sala.devices.push(iluminacao);
+    //sala.devices = [...sala.devices,tomada];
+    //sala.devices = [ar,tv,iluminacao,tomada];
+
+    environments.push(sala);
+
 </script>
 
-<template>  
-    <h1>Devices Page!</h1>
-    {{ sala }}
+<template>
+    <main class="flex flex-column">
+        <h1>Devices Page!</h1>
+        <section class="environments flex flex-colunm">
+
+        </section>
+    </main>
 </template>
 
 <style scoped lang="scss">
-
+    main{
+        width: 100vw;
+        min-width: 100vh;
+        .environments{
+            width: 98;
+        }
+    }
 </style>
